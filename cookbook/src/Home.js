a7395caebedf536ec71f59cb22873472d6aca6fb
Jess <Teurn.j@northeastern.edu>
2023-08-25 14:57:59 -0400

    jt

--- cookbook/src/Home.js
+++ cookbook/src/Home.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import * as React from "react";
+import { Link } from "react-router-dom";
+// Start missing start error
+// npm run
 function Home(){
     return (
         <div>
             <nav>
                 <ul>
-                    <l1><Link to = "/profile">Profile</Link></l1>
-                    <l1><Link to = "/settings">Account Settings</Link></l1>
+                    <l1><Link to ="/profile">Profile</Link></l1>
+                    <l1><Link to ="/settings">Account Settings</Link></l1>
                     <li><a href="/" onClick={() => { /* Log out code here */ }}>Log Out</a></li>
                 </ul>
             </nav>
